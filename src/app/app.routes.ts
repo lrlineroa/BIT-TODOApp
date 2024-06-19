45d28529c2b49e5b8de6214fffa972bdfe6b9bf4
lrlineroa <leonardoraulinero@gmail.com>
2024-06-19 09:43:28 -0500

    feat: created the todo app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodoAppComponent } from './todo-app/todo-app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'todos', component: TodoAppComponent },
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  { path: '**', redirectTo: 'todos', pathMatch: 'full' },
+];
---
+++ src/app/todo-app/todo-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-app.component.html',
+  styleUrl: './todo-app.component.css'
+})
+export class TodoAppComponent {
+
+}
