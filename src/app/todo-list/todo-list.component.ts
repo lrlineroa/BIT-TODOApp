b115072cb21f780680021af6430a9baeb65cbcad
lrlineroa <leonardoraulinero@gmail.com>
2024-06-19 10:58:44 -0500

    feat: add the UI for the list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,19 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrl: './todo-list.component.css',
 })
 export class TodoListComponent {
-
+  records: any[] = [];
+  handleDelete(registro: any) {
+    console.log('delete');
+  }
+  handleEdit(registro: any) {
+    console.log('edit');
+  }
 }
