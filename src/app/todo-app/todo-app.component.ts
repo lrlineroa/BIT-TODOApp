8ff5486579920ea9959b3511162ea726226c37b6
lrlineroa <leonardoraulinero@gmail.com>
2024-06-19 10:10:28 -0500

    feat: created the components for form and list

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TodoListComponent } from '../todo-list/todo-list.component';
+import { TodoFormComponent } from '../todo-form/todo-form.component';
 
 @Component({
   selector: 'app-todo-app',
   standalone: true,
-  imports: [],
+  imports: [TodoListComponent, TodoFormComponent],
   templateUrl: './todo-app.component.html',
-  styleUrl: './todo-app.component.css'
+  styleUrl: './todo-app.component.css',
 })
-export class TodoAppComponent {
-
-}
+export class TodoAppComponent {}
