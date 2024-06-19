ac564e60337f3fe9b20e8599049a303900471be7
lrlineroa <leonardoraulinero@gmail.com>
2024-06-19 10:48:50 -0500

    feat: add the UI for the component form

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,12 +1,28 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './todo-form.component.html',
-  styleUrl: './todo-form.component.css'
+  styleUrl: './todo-form.component.css',
 })
 export class TodoFormComponent {
-
+  registerForm!: FormGroup;
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required]),
+    });
+  }
+  onSubmit(): void {
+    console.log(this.registerForm.value);
+  }
 }
